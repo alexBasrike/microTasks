d8314b15d9e2ce21e983d1761bacdbd611687173
Alexander Dmitriev <alex.basrike@gmail.com>
2026-04-23 10:43:22 +0300

    first work with React Router Dom

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,46 @@
-import {useState} from 'react';
-import './App.css';
-import {Todolist} from './Todolist';
+import styles from "./components/Site.module.css";
+import {PageOne} from "./components/pages/PageOne.tsx";
+import {PageTwo} from "./components/pages/PageTwo.tsx";
+import {PageThree} from "./components/pages/PageThree.tsx";
+import {Link, Navigate, Route, Routes} from "react-router-dom";
+import {Error404} from "./components/pages/Error404.tsx";
 
 export const App = () => {
-    let [tasks, setTasks] = useState([
-        {id: 1, title: "HTML&CSS", isDone: true},
-        {id: 2, title: "JS", isDone: true},
-        {id: 3, title: "ReactJS", isDone: false},
-        {id: 4, title: "Rest API", isDone: false},
-        {id: 5, title: "GraphQL", isDone: false},
-    ]);
-
-    function removeTask(id: number) {
-        let filteredTasks = tasks.filter(t => t.id != id);
-        setTasks(filteredTasks);
-    }
-
-    function removeAllTasks() {
-        setTasks([]);
-    }
-
     return (
-        <div className="App">
-            <Todolist title="What to learn"
-                      tasks={tasks}
-                      removeTask={removeTask}
-                      removeAllTasks={removeAllTasks}
-            />
+        <div>
+
+            <div className={styles.header}>
+                <h1>
+                    <Link to="/">
+                        HEADER
+                    </Link>
+                </h1>
+            </div>
+
+            <div className={styles.body}>
+                <div className={styles.nav}>
+                    <nav>
+                        <ul>
+                            <li><Link to="/page-one">PageOne</Link></li>
+                            <li><Link to="/page-two">PageTwo</Link></li>
+                            <li><Link to="/page-tree">PageThree</Link></li>
+                        </ul>
+                    </nav>
+                </div>
+                <div className={styles.content}>
+                    <Routes>
+                        <Route path="/" element={<Navigate to="/page-one"/>}/>
+
+                        <Route path="/page-one" element={<PageOne/>}/>
+                        <Route path="/page-two" element={<PageTwo/>}/>
+                        <Route path="/page-tree" element={<PageThree/>}/>
+
+                        <Route path="/error404" element={<Error404/>}/>
+                        <Route path="/*" element={<Navigate to="/error404"/>}/>
+                    </Routes>
+                </div>
+            </div>
+            <div className={styles.footer}>abibas 2023</div>
         </div>
     );
 }
